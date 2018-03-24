b7dfb3fb836cf45489f2e0349f003d16c64ea14c
Daniel Brazel <dbrazel4@gmail.com>
2018-03-24 20:01:17 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -50,8 +50,15 @@ end
 def run 
   help
   user_input = ""
-  
   until user_input == "exit"
-      
+    puts "Please enter a command:"
+    user_input = gets.strip
+    if user_input == "list"
+      list(songs)
+    elsif user_input == "play"
+      play(songs)
+    elsif user_input == "exit"
+      exit_jukebox
+    end
   end
 end
